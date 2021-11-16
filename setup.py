b9b0cb1b469811ce07af1b1bc9aceddd2d872910
mstclair@millionconcepts.com <mstclair@millionconcepts.com>
2021-11-16 03:09:23 -0500

    content

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+# this is provided as a convenience for automated install
+# hooks. we do not recommend using this file to install
+# marslab or its dependencies. please use conda along with
+# the provided environment.yml file.
+
+setup(
+    name="silencio",
+    version="0.1.0",
+    url="https://github.com/millionconcepts/silencio.git",
+    author="Million Concepts",
+    author_email="mstclair@millionconcepts.com",
+    description="Occasional networking API interfaces.",
+    packages=find_packages(),
+    python_requires=">=3.9",
+    install_requires=[
+        "pip", "pydrive2", "python-dateutil"
+    ],
+)
